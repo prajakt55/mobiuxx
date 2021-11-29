81c69a7d50761160329574eeab4c3442f18ecd13
prajakt chatur <85507121+prajakt55@users.noreply.github.com>
2021-11-29 09:35:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const userData = [
+    {name : 'prajakt', sirname: 'chatur'},
+    {name : 'manas', sirname: 'chatur'},
+    {name : 'prachi', sirname: 'chatur'},
+    {name : 'manas', sirname: 'chatur'}
+]
